77ae77a44e5d6dc72953a87d19eb98f4864f4e5a
Tivka8901 <danilmiler8901@gmail.com>
2024-10-23 00:15:18 +0300

    поправки brain-calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,7 +6,7 @@ DESCRIPTION = "What is the result of the expression?"
 OPERATIONS = {
     '+': operator.add,
     '-': operator.sub,
-    '*': operator.mul
+    '*': operator.mul,
 }
 
 def generate_question_answer():
@@ -18,5 +18,26 @@ def generate_question_answer():
     correct_answer = str(OPERATIONS[operation](number1, number2))
 
     return question, correct_answer
+
+def run_game():
+    print(DESCRIPTION)
+    rounds = 3
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+
+    for _ in range(rounds):
+        question, correct_answer = generate_question_answer()
+        print(f"Question: {question}")
+        user_answer = input("Your answer: ")
+
+        if user_answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
 
 
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,28 +1,7 @@
-from brain_games.games.calc import generate_question_answer
-
-DESCRIPTION = "What is the result of the expression?"
-
-
-def run_game(game_function):
-    print(DESCRIPTION)
-
-    for _ in range(3):
-        question, correct_answer = game_function()
-        print(f"Question: {question}")
-
-        user_answer = input("Your answer: ")
-        if user_answer == correct_answer:
-            print("Correct!")
-        else:
-            print(f"Wrong! The correct answer was {correct_answer}.")
-            break
-    else:
-        print("Congratulations, you answered all questions correctly!")
-
+from brain_games.games.calc import run_game
 
 def main():
-    run_game(generate_question_answer)
-
+    run_game()
 
 if __name__ == "__main__":
     main()
